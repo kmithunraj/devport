f78b8d16331ef289f831a7fa516870e04b6c3b5a
Mithun Raj <kmithunraj02@gmail.com>
2022-10-13 23:54:47 +0530

    web updatess

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function navBtn() {
       x.style.display = "none";
     }
   }
+
+
+
+  function validate(e)
+    {
+      e.preventDefault();
+
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        var loginlist = { "User": {
+           "userlist":[{"username":"kmithunraj@gmail.com", "password":"hello"},{"username":"jeevanantham", "password":"hellosir"},{"username":"usr3", "password":"a123"}],
+                                  }
+        };
+        
+            for(var i =0; i < loginlist.User.userlist.length; i++){
+                if(username == loginlist.User.userlist[i].username && password == loginlist.User.userlist[i].password){
+                    alert("User login");
+                    window.location.href("https://mithunraj.com/devport/dashboard.html");
+                }
+                else{
+                  alert('Wrong username or password');
+                  return false;
+              }
+            }
+          }
+  
+          document.getElementById('formLogin').addEventListener('button', validate);
