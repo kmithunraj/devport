11a40f104223a68c3058810ae7c04951253c79bf
Mithun Raj <kmithunraj02@gmail.com>
2022-10-19 23:06:13 +0530

    web updatess

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const loginlist = { "User": {
+
+const userlist = { "User": {
     "userlist":[{"username":"kmithunraj@gmail.com", "password":"hello"},{"username":"jeevanantham", "password":"hellosir"},{"username":"usr3", "password":"a123"}],
                            }
  };
@@ -13,9 +14,9 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-for(const i =0; i < loginlist.User.userlist.length; i++){
-    if(username == loginlist.User.userlist[i].username && password == loginlist.User.userlist[i].password){
-        window.location.href="https://mithunraj.com/devport/dashboard.html";
+for(const i =0; i < userlist.User.userlist.length; i++){
+    if(username == userlist.User.userlist[i].username && password == userlist.User.userlist[i].password){
+        location.href="https://mithunraj.com/devport/dashboard.html";
     }
     else{
       alert('Wrong username or password');
