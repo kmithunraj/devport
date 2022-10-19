9634cdf37d99ba4fb541baf5a4f4a14b2c9c72ad
Mithun Raj <kmithunraj02@gmail.com>
2022-10-19 21:52:29 +0530

    login system

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
-function navBtn() {
-    var x = document.getElementById("navbar");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+const loginlist = { "User": {
+    "userlist":[{"username":"kmithunraj@gmail.com", "password":"hello"},{"username":"jeevanantham", "password":"hellosir"},{"username":"usr3", "password":"a123"}],
+                           }
+ };
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+for(const i =0; i < loginlist.User.userlist.length; i++){
+    if(username == loginlist.User.userlist[i].username && password == loginlist.User.userlist[i].password){
+        window.location.href="dashboard.html";
     }
+    else{
+      alert('Wrong username or password');
+      return false;
   }
-
-
-
-  function validate(e)
-    {
-      e.preventDefault();
-
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        var loginlist = { "User": {
-           "userlist":[{"username":"kmithunraj@gmail.com", "password":"hello"},{"username":"jeevanantham", "password":"hellosir"},{"username":"usr3", "password":"a123"}],
-                                  }
-        };
-        
-            for(var i =0; i < loginlist.User.userlist.length; i++){
-                if(username == loginlist.User.userlist[i].username && password == loginlist.User.userlist[i].password){
-                    alert("User login");
-                    window.location.href("https://mithunraj.com/devport/dashboard.html");
-                }
-                else{
-                  alert('Wrong username or password');
-                  return false;
-              }
-            }
-          }
-  
-          document.getElementById('formLogin').addEventListener('button', validate);
+}})
